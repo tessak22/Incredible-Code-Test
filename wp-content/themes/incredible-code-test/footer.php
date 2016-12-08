41b75e5b3b6aaccc5f99fff5b4d00f9fe846eae8
Tessa Kriesel <me@tessak22.com>
2016-12-08 14:07:27 -0600

    Bug Fix: On responsive menu open, bring to top of window.

--- wp-content/themes/incredible-code-test/footer.php
+++ wp-content/themes/incredible-code-test/footer.php
@@ -46,13 +46,14 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <script type="text/javascript">
-	//add or remove nav-open class to body for off-canvas nav
     $('.nav-toggle').click(function() {
         $('body').toggleClass("nav-open");
     });
-</script>
-<script type="text/javascript">
-	//add or remove nav-open class to body for off-canvas nav
+
+    $(".nav-toggle").click(function(){
+          $('html, body').animate({ scrollTop: 0 }, 'fast');
+	})
+
     $('.close-menu').click(function() {
         $('body').toggleClass("nav-open");
     });
